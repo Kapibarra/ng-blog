ece64489dce3e9430730d84c3ba9340c3e4d1f47
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-03-11 12:38:45 +0300

    routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomepagesComponent } from "./homepages/homepages.component";
+import { PostPageComponent } from "./post-page/post-page.component";
+import { MainLayoutComponent } from "./shared/components/main-layout/main-layout.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainLayoutComponent, children: [
+    {path: '', redirectTo: '/', pathMatch: 'full'},
+    {path:'', component: HomepagesComponent},
+    {path:'post/:id', component: PostPageComponent}
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomepagesComponent } from './homepages/homepages.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomepagesComponent,
+    PostPageComponent
   ],
   imports: [
     BrowserModule,
