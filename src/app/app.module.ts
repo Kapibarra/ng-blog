e4da9f9ca5f59bc4f6cb19fdfd1fb5b221df6bc3
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-03-11 14:17:11 +0300

    post component render

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomepagesComponent } from './homepages/homepages.component';
 import { PostPageComponent } from './post-page/post-page.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
     HomepagesComponent,
-    PostPageComponent
+    PostPageComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
