61e278586044014a77d4dd3877993f81cad2ead1
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-03-14 17:32:52 +0300

    8 enter page p1

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,19 +3,37 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { AdminLayoutComponent } from "./shared/components/admin-layout/admin-layout.component";
-import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
-import { CreatePageComponent } from './create-page/create-page.component';
-import { EditPageComponent } from './edit-page/edit-page.component';
+import { DashboardPageComponent } from "./dashboard-page/dashboard-page.component";
+import { CreatePageComponent } from "./create-page/create-page.component";
+import { EditPageComponent } from "./edit-page/edit-page.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
-  imports: [CommonModule, RouterModule.forChild([{ path: '', component: AdminLayoutComponent, children: [
-      {path:'', redirectTo: '/admin/login', pathMatch: 'full'},
-      {path:'login', component: LoginPageComponent},
-      {path: 'dashboard', component: DashboardPageComponent},
-      {path: 'create', component: CreatePageComponent},
-      {path: 'posts/:id/edit', component: EditPageComponent},
-  ]}])],
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      {
+        path: "",
+        component: AdminLayoutComponent,
+        children: [
+          { path: "", redirectTo: "/admin/login", pathMatch: "full" },
+          { path: "login", component: LoginPageComponent },
+          { path: "dashboard", component: DashboardPageComponent },
+          { path: "create", component: CreatePageComponent },
+          { path: "posts/:id/edit", component: EditPageComponent },
+        ],
+      },
+    ]),
+  ],
   exports: [RouterModule],
 })
 export class AdminModule {}
--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
+form: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.minLength(6), Validators.required]),
+    });
   }
 
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+  }
 }
